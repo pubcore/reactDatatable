2e49a3a6a840a7a08354b5f34d14f12b0e0313b7
jacklrs <github@rushsoft.de>
2020-05-03 18:48:04 +0200

    update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,42 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true,
-        "mocha": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "mocha"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ],
-        "react/jsx-uses-react": 2,
-		"react/jsx-uses-vars": "error"
-    }
+  "env": {
+      "browser": true,
+      "es6": true,
+      "node": true,
+      "mocha": true
+  },
+  "extends": [
+		'eslint:recommended',
+		'plugin:react/recommended'
+	],
+  'parser': "babel-eslint",
+  "plugins": [
+      "react",
+      "mocha"
+  ],
+  "rules": {
+      "indent": [
+          "error",
+          "tab"
+      ],
+      "linebreak-style": [
+          "error",
+          "unix"
+      ],
+      "quotes": [
+          "error",
+          "single"
+      ],
+      "semi": [
+          "error",
+          "never"
+      ],
+      "react/jsx-uses-react": 2,
+			"react/jsx-uses-vars": "error"
+	},
+	'settings': {
+		'react': {
+			'version': 'detect'
+		}
+	}
 };
--- src/view/Rowcount.js
+++ src/view/Rowcount.js
@@ -1,20 +1,19 @@
 import PT from 'prop-types'
-import uiText from 'pubcore-ui-text'
+import uiText from '@pubcore/ui-text'
 
-$.propTypes = {
+Rowcount.propTypes = {
 	rowCount:PT.number.isRequired,
 	total:PT.number.isRequired,
 	offset:PT.number.isRequired,
 	T:PT.object.isRequired
 }
-$.displayName = 'Rowcount'
-export default function $({rowCount, total, offset, T}) { return 0||
+export default function Rowcount({rowCount, total, offset, T}) { return 0||
 	rowCount ?
 	uiText(T, 'number_of_entries', 'Showing {first} to {last} of {num} entries', {
 		first: offset + 1,
 		last: offset + rowCount,
 		num: total > rowCount ? total : rowCount
-	})
+	}, T.__config)
 	:
-	uiText(T, 'no_entries', 'Not entries found')
+	uiText(T, 'no_entries', 'Not entries found', null, T.__config)
 }
