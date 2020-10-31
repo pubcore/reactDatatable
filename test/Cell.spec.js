d32d7a04c1618090fc74a94b38947f37113c02f3
jacklrs <github@rushsoft.de>
2020-10-31 19:03:45 +0100

    test: render cell with undefined data;

--- test/Cell.spec.js
+++ test/Cell.spec.js
@@ -9,14 +9,18 @@ import Cell from '../src/view/Cell'
 describe('<Cell/>', () => {
 	it('support className', () => {
 		const wrapper = shallow(<Cell className="foo" />)
-		expect(wrapper.hasClass('foo')).to.equal(true)
+		expect(wrapper.hasClass('foo')).to.be.true
 	})
 	it('support individual children', () => {
 		const wrapper = shallow(<Cell><span/></Cell>)
-		expect(wrapper.contains(<span/>))
+		expect(wrapper.contains(<span/>)).to.be.true
 	})
 	it('shows json for data of type object as default', () => {
 		const wrapper = shallow(<Cell data={{foo:'bar'}}/>)
-		expect(wrapper.contains('"foo":"bar"'))
+		expect(wrapper.contains('{"foo":"bar"}')).to.be.true
+	})
+	it('shows empty for undefined', () => {
+		const wrapper = shallow(<Cell data={undefined}/>)
+		expect(wrapper.contains(<td></td>)).to.be.true
 	})
 })
